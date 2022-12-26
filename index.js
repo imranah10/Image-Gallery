70a3f6e451be0f7ed4872602b9555565dc17dd01
imran <imranaha310@gmail.com>
2022-12-26 22:15:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var gallery = (x) => {
+    var Main = document.getElementById("main").src=x.src;
+    x.style.border="10px groove red"
+    x.style.transform="scale(1.3)"
+    x.style.transition="1s"
+  };
+  
+
+  var gallery1 = (y) => {
+    var Main=document.getElementById('main').src="1.jpg"
+    y.style.border="10px groove yellowgreen"
+    y.style.transform="scale(1)"
+    y.style.transition="1s"
+  };
+  
